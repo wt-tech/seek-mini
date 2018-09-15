026274eb7b6fdba0d4a19a7685d0848a2d35028c
李刚 <867982914@QQ.com>
2018-09-15 17:45:58 +0800

    banner详情

--- pages/bannerdetail/bannerdetail.js
+++ pages/bannerdetail/bannerdetail.js
@@ -9,7 +9,6 @@ Page({
   onLoad: function (options) {
     let bannerId = options.bannerId;
     let src = 'http://127.0.0.1:8848/seek-back/banner/detail.html?bannerId='+bannerId;
-
     this.setData({
       bannerId : bannerId,
       src : src
