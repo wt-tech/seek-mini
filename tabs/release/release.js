2b0719389e7ff08288f7b201d3a6ab6b172317e8
李刚 <867982914@QQ.com>
2018-09-29 09:11:41 +0800

    样式调整

--- tabs/release/release.js
+++ tabs/release/release.js
@@ -5,32 +5,117 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+
+    animationData:{},
+    animationData2: {},
+    checkOpacity:0,
+    checkBtnOpaity:1,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
-  },
+    let animation = wx.createAnimation({
+      duration: 500,
+      timingFunction: "ease",
+      delay: 0,
+    });
+    animation.opacity(1).translateY(60).scale(1.1).backgroundColor('#e1e1e1').step();
+    let animation2 = wx.createAnimation({
+      // transformOrigin:"50% 50%",
+      duration: 500,
+      timingFunction: "ease",
+      delay: 0,
+    });
+    animation2.opacity(1).translateY(0).scale(1).backgroundColor('#ffffff').step();
 
-
-  searchPeople:function(){
-    wx.navigateTo({
-      url: './releaseNotes/releaseNotes?searchPeople=true',
+    this.setData({
+      checkBtnOpacity: 0,
+      bgcolor: true,
+      animationData: animation2.export(),
+      animationData2: animation.export()
     })
-  },
 
-  searchHome:function(){
-    wx.navigateTo({
-      url: './releaseNotes/releaseNotes?searchPeople=false',
-    })
   },
 
 
+  // searchPeople:function(){
+  //   wx.navigateTo({
+  //     url: './releaseNotes/releaseNotes?searchPeople=true',
+  //   })
+  // },
+
+  // searchHome:function(){
+  //   wx.navigateTo({
+  //     url: './releaseNotes/releaseNotes?searchPeople=false',
+  //   })
+  // },
+
+  bindcheck:function(e){
+    this.animation()
+   
+  },
 
 
+  bindcheck2: function (e) {
+    this.animation2()
+  },
+
+animation:function(){
+  let animation = wx.createAnimation({
+    // transformOrigin:"50% 50%",
+    duration: 500,
+    timingFunction: "ease",
+    delay: 0,
+  });
+  animation.opacity(1).translateY(-60).scale(1.1).backgroundColor('#e1e1e1').step();
+
+  let animation2 = wx.createAnimation({
+    duration: 500,
+    timingFunction: "ease",
+    delay: 0,
+  });
+  animation2.opacity(1).translateY(0).scale(1).backgroundColor('#ffffff').step();
+
+  this.setData({
+    checkBtnOpacity: 0,
+    animationData2: animation2.export(),
+    animationData: animation.export()
+  })
+    // setTimeout(function(){
+    //   wx.navigateTo({
+    //     url: './releaseNotes/releaseNotes?searchPeople=false',
+    //   })
+    // }, 600)
+},
+animation2:function(){
+  let animation = wx.createAnimation({
+    duration: 500,
+    timingFunction: "ease",
+    delay: 0,
+    
+  });
+  animation.opacity(1).translateY(60).scale(1.1).backgroundColor('#e1e1e1').step();
+  let animation2 = wx.createAnimation({
+    // transformOrigin:"50% 50%",
+    duration: 500,
+    timingFunction: "ease",
+    delay: 0,
+  });
+  animation2.opacity(1).translateY(0).scale(1).backgroundColor('#ffffff').step();
+
+  this.setData({
+    checkBtnOpacity: 0,
+    animationData: animation2.export(),
+    animationData2: animation.export()
+  })
+    // setTimeout(function () {
+    //   wx.navigateTo({
+    //     url: './releaseNotes/releaseNotes?searchPeople=true',
+    //   })
+    // }, 600)
+},
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
