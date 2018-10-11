dac97811e35927d8f9e1710be82740760890853d
李刚 <867982914@QQ.com>
2018-10-11 10:54:50 +0800

    canvas执行顺序修改

--- pages/details/details.js
+++ pages/details/details.js
@@ -57,7 +57,6 @@ Page({
     that.getmark(params) 
     that.browse()   //向后台插入浏览记录
 
-    that.pageImg()
   },
 
 // 绘制页面二维码
@@ -85,7 +84,7 @@ token:function(a){
       scene: id,
       page:'pages/details/details',
     },
-    method: "POST",
+    method: "GET",
     header: {
       'Content-Type': 'application/x-www-form-urlencoded'
     },
@@ -102,6 +101,8 @@ token:function(a){
       that.setData({
         src:  "data:image/png;base64,"+src
       })
+      // 当二维码的请求全部结束之后调用canvas绘制图片
+      that.shareImg2()
     },
     fail: function (err) {
       console.log(err)
@@ -216,132 +217,120 @@ token:function(a){
     })
   },
 
-// 分享
+// 分享，开始请求二维码
   shareImg:function(){
     let that = this
-    const ctx = wx.createCanvasContext('myCanvas')
-    // 获取当前的信息
-
-    let content = that.data.content
-    
-    let name = content.missName
-    let missdata = content.missDate
-    let missadd = content.missDetailPlace
-    let feature = content.feature
-
-
-
-    // 设置背景颜色以及图片大小位置
-    ctx.setFillStyle('#fff')
-    ctx.fillRect(0, 0, 360, 600)
-    // ctx.setStrokeStyle("#00ff00")
-    ctx.rect(0, 0, 360, 560)
-
-    // 获取图片，没有就绘制指定的图片
-    if (that.data.imgPath){
-      ctx.drawImage(that.data.imgPath, 0, 0, 360, 360)
-    }else{
-      ctx.drawImage('../../resource/img/searchLogo3.jpg', 0, 0, 360, 360)
-    }
-    // 填充小程序码
-    // ctx.drawImage('../../resource/img/hbxr.jpg', 15, 505, 90, 90)
-
-
-
-    // 填充文本
-    ctx.setFontSize(14)
-    ctx.setFillStyle('#000000')
-    ctx.fillText('姓名：'+name, 15, 390)
-    ctx.fillText('失踪时间：' + missdata, 15, 410)
-    ctx.fillText('失踪地点：' + missadd, 15, 430)
-    ctx.fillText('相貌特征：', 15, 450)
-
-    ctx.fillText('扫码关注，帮助更多人与家人团聚', 105, 550)
-
-
-    var text = feature    //这是要绘制的文本
-    var chr = text.split("")
+    that.pageImg()
   
+  },
+// 绘制canvas
+shareImg2:function(){
+  var that = this
+  const ctx = wx.createCanvasContext('myCanvas')
+  // 获取当前的信息
+
+  let content = that.data.content
+
+  let name = content.missName
+  let missdata = content.missDate
+  let missadd = content.missDetailPlace
+  let feature = content.feature
 
 
 
+  // 设置背景颜色以及图片大小位置
+  ctx.setFillStyle('#fff')
+  ctx.fillRect(0, 0, 360, 600)
+  // ctx.setStrokeStyle("#00ff00")
+  ctx.rect(0, 0, 360, 560)
+
+  // 获取图片，没有就绘制指定的图片
+  if (that.data.imgPath) {
+    ctx.drawImage(that.data.imgPath, 0, 0, 360, 360)
+  } else {
+    ctx.drawImage('../../resource/img/searchLogo3.jpg', 0, 0, 360, 360)
+  }
+  // 填充小程序码
+  // ctx.drawImage('../../resource/img/hbxr.jpg', 15, 505, 90, 90)
 
 
-    var temp = ""
-    var row = []
-    for (var a = 0; a < chr.length; a++) {
-      if (ctx.measureText(temp).width < 260) {
-        temp += chr[a];
+
+  // 填充文本
+  ctx.setFontSize(14)
+  ctx.setFillStyle('#000000')
+  ctx.fillText('姓名：' + name, 15, 390)
+  ctx.fillText('失踪时间：' + missdata, 15, 410)
+  ctx.fillText('失踪地点：' + missadd, 15, 430)
+  ctx.fillText('相貌特征：', 15, 450)
+
+  ctx.fillText('扫码关注，帮助更多人与家人团聚', 105, 550)
+
+
+  var text = feature    //这是要绘制的文本
+  var chr = text.split("")
+
+  var temp = ""
+  var row = []
+  for (var a = 0; a < chr.length; a++) {
+    if (ctx.measureText(temp).width < 260) {
+      temp += chr[a];
+    }
+    else {
+      a--; //这里添加了a-- 是为了防止字符丢失，
+      row.push(temp);
+      temp = "";
+    }
+  }
+  row.push(temp);
+  //如果数组长度大于2 则截取前两个
+  //2取前两行3取前三行
+  if (row.length > 2) {
+    var rowCut = row.slice(0, 3);
+    var rowPart = rowCut[1];
+    var test = "";
+    var empty = [];
+    for (var a = 0; a < rowPart.length; a++) {
+      if (ctx.measureText(test).width < 220) {
+        test += rowPart[a];
       }
       else {
-        a--; //这里添加了a-- 是为了防止字符丢失，
-        row.push(temp);
-        temp = "";
+        break;
       }
     }
-    row.push(temp);
-    //如果数组长度大于2 则截取前两个
-    //2取前两行3取前三行
-    if (row.length > 2) {
-      var rowCut = row.slice(0, 3);
-      var rowPart = rowCut[1];
-      var test = "";
-      var empty = [];
-      for (var a = 0; a < rowPart.length; a++) {
-        if (ctx.measureText(test).width < 220) {
-          test += rowPart[a];
-        }
-        else {
-          break;
-        }
+    empty.push(test);
+    // var group = empty[0] + "..."//这里只显示两行，超出的用...表示
+    // rowCut.splice(2, 2, group);//1,1在第二行开始有省略号。2,2在第三行有省略号
+    row = rowCut;
+  }
+  for (var b = 0; b < row.length; b++) {
+    ctx.fillText(row[b], 75, 450 + b * 20);
+  }
+
+  // 绘制当前页面的二维码
+  const src = that.data.src;
+  ctx.drawImage(src, 10, 500, 90, 90)
+
+  ctx.draw(true, setTimeout(function () {
+    wx.canvasToTempFilePath({
+      x: 0,
+      y: 0,
+      width: 500,
+      height: 800,
+      destWidth: 1500,
+      destHeight: 2400,
+      canvasId: 'myCanvas',
+      success: function (res) {
+        that.setData({
+          filePath: res.tempFilePath,
+          markHide: true
+        })
+
       }
-      empty.push(test);
-      // var group = empty[0] + "..."//这里只显示两行，超出的用...表示
-      // rowCut.splice(2, 2, group);//1,1在第二行开始有省略号。2,2在第三行有省略号
-      row = rowCut;
-    }
-    for (var b = 0; b < row.length; b++) {
-      ctx.fillText(row[b], 75, 450 + b * 20);
-    }
-
-// 绘制当前页面的二维码
-
-    const src = that.data.src;  
-
-  
-    ctx.drawImage(src, 0, 0, 200, )
-   
+    })
+  }, 100))
+},
 
 
-
-
-    // console.log(src)
-
-
-
-    ctx.draw(true, setTimeout(function () {
-      wx.canvasToTempFilePath({
-        x: 0,
-        y: 0,
-        width: 500,
-        height: 800,
-        destWidth: 1500,
-        destHeight: 2400,
-        canvasId: 'myCanvas',
-        success: function (res) {
-          that.setData({
-            filePath: res.tempFilePath,
-            markHide:true
-          })
-
-          // let id = that.data.id
-          // wx.navigateTo({
-          //   url: '../share/share?filePath=' + res.tempFilePath + '&id='+id,
-          // })
-        }
-      })
-    }, 100))
-  },
 
 // 保存图片到相册
   saveImg: function () {
